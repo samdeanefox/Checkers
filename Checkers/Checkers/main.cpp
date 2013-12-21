d447b8d5e53231e8ec193ac2438d08f4984e0dd7
Sam Fox <Sam@Sam-Foxs-MacBook-Pro.local>
2013-12-20 19:06:09 -0800

    initial

---
+++ Checkers/Checkers/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Checkers
+//
+//  Created by Sam Fox on 12/20/13.
+//  Copyright (c) 2013 Sam Fox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
